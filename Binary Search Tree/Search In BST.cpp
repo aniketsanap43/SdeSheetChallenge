2ea232eed39568add9264a97a5752b484f98b8ef
Aniket Sanap <72439307+aniketsanap43@users.noreply.github.com>
2022-06-14 12:55:43 +0530

    Create Search In BST.cpp

---
+++ Binary Search Tree/Search In BST.cpp
@@ -0,0 +1,10 @@
+bool searchInBST(BinaryTreeNode<int> *root, int x) {
+    // Write your code here.
+    if(root==NULL)
+        return false;
+    if(root->data==x)
+        return true;
+    if(x<root->data)
+        return searchInBST(root->left,x);
+    return searchInBST(root->right,x);
+}
